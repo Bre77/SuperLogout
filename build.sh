b1ad37e1a28937207863612a95115ce6fe0f5c33
Brett Adams <brett.whynot@gmail.com>
2023-06-24 22:00:21 +1000

    Build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "${0%/*}"
+OUTPUT="${1:-super_logout.spl}"
+yarn install --non-interactive
+yarn run build
+chmod -R u=rwX,go= stage/*
+chmod -R u-x+X stage/*
+chmod -R u=rwx,go= stage/bin/*
+mv stage super_logout
+tar -cpzf $OUTPUT --exclude=super_logout/.* --overwrite super_logout 
+rm -rf super_logout
